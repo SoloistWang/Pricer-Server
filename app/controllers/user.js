b12a5c5c02fac74d0f146cf94000fb81f10d3ebc
soloistwang <342916053@qq.com>
2019-04-12 00:13:38 +1000

    with back end funcion

--- app/controllers/user.js
+++ app/controllers/user.js
@@ -12,9 +12,8 @@ var userHelper = require('../dbhelper/userHelper')
  * @param {Function} next          [description]
  * @yield {[type]}   [description]
  */
-exports.signup = async (ctx, next) => {////
+exports.signup = async (ctx, next) => {
   console.log(ctx.request.body)
-
 	var phoneNumber = xss(ctx.request.body.phoneNumber)
 	var user = await User.findOne({
 	  phoneNumber: phoneNumber
@@ -28,7 +27,6 @@ exports.signup = async (ctx, next) => {////
 
 	  user = new User({
 	    nickname: '测试用户',
-	    avatar: 'http://upload-images.jianshu.io/upload_images/5307186-eda1b28e54a4d48e.png?imageMogr2/auto-orient/strip%7CimageView2/2/w/1240',
 	    phoneNumber: xss(phoneNumber),
 	    verifyCode: verifyCode,
 	    accessToken: accessToken
--- app/models/user.js
+++ app/models/user.js
@@ -11,22 +11,17 @@ var Schema = mongoose.Schema;
  * @type {mongoose}
  */
 var UserSchema = new Schema({
-	phoneNumber: {
+	username: {
     unique: true,
     type: String
   },
-  areaCode: String,
   verifyCode: String,
   verified: {
     type: Boolean,
     default: false
   },
   accessToken: String,
-  nickname: String,
-  gender: String,
-  breed: String,
-  age: String,
-  avatar: String,
+  password:String,
   meta: {
     createAt: {
       type: Date,
